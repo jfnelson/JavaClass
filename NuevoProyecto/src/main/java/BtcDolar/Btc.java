e76b41cde9b76f6fa07cf4fbabc3b3d65703c95a
jfnelson <90727208+jfnelson@users.noreply.github.com>
2021-10-14 14:37:18 -0600

    Add files via upload

---
+++ NuevoProyecto/src/main/java/BtcDolar/Btc.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BtcDolar;
+
+import java.awt.Panel;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Btc {
+    
+    public static void main (String[] args)
+    
+    {
+        Panel j=new Panel();
+        j.setVisible(true);
+        
+    }
+    
+}
